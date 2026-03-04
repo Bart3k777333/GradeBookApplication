22d710763d305c0d7f04c68ccef10145aa49cd03
Bartek K <bart3kgithub@gmail.com>
2026-03-04 15:55:45 +0100

    CalculateStatistics() and CalculateStudentStatistics(string name)

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,4 +29,26 @@ public class RankedGradeBook : BaseGradeBook
 
         return 'F';
     }
+
+    public override void CalculateStatistics()
+    {
+        if (Students.Count < 5)
+        {
+            Console.WriteLine("Ranked grading requires at least 5 students.");
+            return;
+        }
+
+        base.CalculateStatistics();
+    }
+
+    public override void CalculateStudentStatistics(string name)
+    {
+        if (Students.Count < 5)
+        {
+            Console.WriteLine("Ranked grading requires at least 5 students.");
+            return;
+        }
+
+        base.CalculateStudentStatistics(name);
+    }
 }
