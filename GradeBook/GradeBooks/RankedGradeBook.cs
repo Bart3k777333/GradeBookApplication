cd155cc2526c7219133b65b3d1efd1dabe45985e
Bartek K <bart3kgithub@gmail.com>
2026-03-04 15:43:48 +0100

    GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks;
 
@@ -7,5 +9,24 @@ public class RankedGradeBook : BaseGradeBook
     public RankedGradeBook(string name) : base(name)
     {
         Type = GradeBookType.Ranked;
+    }
+
+    public override char GetLetterGrade(double averageGrade)
+    {
+        if (Students.Count < 5)
+        {
+            throw new InvalidOperationException();
+        }
+
+        int students20Percent = Students.Count / 5;
+
+        int higherGradesCount = Students.Count(s => s.AverageGrade >  averageGrade);
+
+        if (higherGradesCount < students20Percent) return 'A';
+        if (higherGradesCount < students20Percent * 2) return 'B';
+        if (higherGradesCount < students20Percent * 3) return 'C';
+        if (higherGradesCount < students20Percent * 4) return 'D';
+
+        return 'F';
     }
 }
